de800e9119f76b90ae1068876c8a2368f8bf722e
lvan100 <lvan100@yeah.net>
2020-06-28 22:27:17 +0800

    Go-Spring 初步集成成功!

--- server/core/server.go
+++ server/core/server.go
@@ -1,20 +1,51 @@
 package core
 
 import (
-	"fmt"
+	"errors"
+	"net/http"
+
 	"gin-vue-admin/global"
 	"gin-vue-admin/initialize"
-	"net/http"
-	"time"
+
+	"github.com/go-spring/go-spring-web/spring-gin"
+	"github.com/go-spring/go-spring-web/spring-web"
+	"github.com/go-spring/go-spring/spring-boot"
+	"github.com/go-spring/go-spring/starter-web"
 )
+
+func init() {
+	SpringBoot.RegisterBeanFn(func(config WebStarter.WebServerConfig) SpringWeb.WebContainer {
+		c := SpringGin.NewContainer(SpringWeb.ContainerConfig{Port: config.Port})
+		c.SetEnableSwagger(false)
+		return c
+	})
+}
 
 func RunWindowsServer() {
 	if global.GVA_CONFIG.System.UseMultipoint {
 		// 初始化redis服务
 		initialize.Redis()
 	}
+
 	Router := initialize.Routers()
 	Router.Static("/form-generator", "./resource/page")
+
+	for _, info := range Router.Routes() {
+		switch info.Method {
+		case http.MethodGet:
+			SpringBoot.GET(info.Path, SpringGin.Gin(info.HandlerFunc))
+		case http.MethodPost:
+			SpringBoot.POST(info.Path, SpringGin.Gin(info.HandlerFunc))
+		case http.MethodDelete:
+			SpringBoot.DELETE(info.Path, SpringGin.Gin(info.HandlerFunc))
+		case http.MethodPut:
+			SpringBoot.PUT(info.Path, SpringGin.Gin(info.HandlerFunc))
+		case http.MethodHead:
+			SpringBoot.HEAD(info.Path, SpringGin.Gin(info.HandlerFunc))
+		default:
+			panic(errors.New("unsupported http method " + info.Method))
+		}
+	}
 
 	// 插件安装 暂时只是后台功能 添加model 添加路由 添加对数据库的操作  详细插件测试模板可看https://github.com/piexlmax/gvaplug  此处不建议投入生产
 	//err := initialize.InstallPlug(global.GVA_DB, Router, gvaplug.GvaPlug{})
@@ -23,22 +54,24 @@ func RunWindowsServer() {
 	//}
 	// end 插件描述
 
-	address := fmt.Sprintf(":%d", global.GVA_CONFIG.System.Addr)
-	s := &http.Server{
-		Addr:           address,
-		Handler:        Router,
-		ReadTimeout:    10 * time.Second,
-		WriteTimeout:   10 * time.Second,
-		MaxHeaderBytes: 1 << 20,
-	}
+	//address := fmt.Sprintf(":%d", global.GVA_CONFIG.System.Addr)
+	//s := &http.Server{
+	//	Addr:           address,
+	//	Handler:        Router,
+	//	ReadTimeout:    10 * time.Second,
+	//	WriteTimeout:   10 * time.Second,
+	//	MaxHeaderBytes: 1 << 20,
+	//}
+
 	// 保证文本顺序输出
 	// In order to ensure that the text order output can be deleted
-	time.Sleep(10 * time.Microsecond)
-	global.GVA_LOG.Debug("server run success on ", address)
+	//time.Sleep(10 * time.Microsecond)
+	//global.GVA_LOG.Debug("server run success on ", address)
 
-	fmt.Printf(`欢迎使用 Gin-Vue-Admin
-	默认自动化文档地址:http://127.0.0.1%s/swagger/index.html
-	默认前端文件运行地址:http://127.0.0.1:8080
-`, s.Addr)
-	global.GVA_LOG.Error(s.ListenAndServe())
+	//fmt.Printf(`欢迎使用 Gin-Vue-Admin
+	//默认自动化文档地址:http://127.0.0.1%s/swagger/index.html
+	//默认前端文件运行地址:http://127.0.0.1:8080`, s.Addr)
+
+	// global.GVA_LOG.Error(s.ListenAndServe())
+	SpringBoot.RunApplication("./")
 }
--- server/go.mod
+++ server/go.mod
@@ -11,15 +11,17 @@ require (
 	github.com/fastly/go-utils v0.0.0-20180712184237-d95a45783239 // indirect
 	github.com/fsnotify/fsnotify v1.4.9
 	github.com/gin-gonic/gin v1.6.3
-	github.com/go-openapi/spec v0.19.7 // indirect
 	github.com/go-openapi/swag v0.19.8 // indirect
 	github.com/go-playground/validator/v10 v10.3.0 // indirect
 	github.com/go-redis/redis v6.15.7+incompatible
+	github.com/go-spring/go-spring v1.0.4
+	github.com/go-spring/go-spring-web v1.0.5-0.20200628140552-c269f717f387
 	github.com/go-sql-driver/mysql v1.5.0 // indirect
 	github.com/golang/protobuf v1.4.2 // indirect
 	github.com/jehiah/go-strftime v0.0.0-20171201141054-1d33003b3869 // indirect
 	github.com/jinzhu/gorm v1.9.12
 	github.com/json-iterator/go v1.1.10 // indirect
+	github.com/kr/pty v1.1.5 // indirect
 	github.com/lestrrat/go-envload v0.0.0-20180220120943-6ed08b54a570 // indirect
 	github.com/lestrrat/go-file-rotatelogs v0.0.0-20180223000712-d3151e2a480f
 	github.com/lestrrat/go-strftime v0.0.0-20180220042222-ba3bf9c1d042 // indirect
@@ -30,16 +32,16 @@ require (
 	github.com/onsi/gomega v1.4.3 // indirect
 	github.com/op/go-logging v0.0.0-20160315200505-970db520ece7
 	github.com/pelletier/go-toml v1.6.0 // indirect
-	github.com/piexlmax/gvaplug v0.0.5
+	github.com/piexlmax/gvaplug v0.0.5 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
 	github.com/qiniu/api.v7 v7.2.5+incompatible
 	github.com/qiniu/x v7.0.8+incompatible // indirect
 	github.com/satori/go.uuid v1.2.0
 	github.com/spf13/afero v1.2.2 // indirect
-	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.6.2
+	github.com/spf13/viper v1.6.3
+	github.com/stretchr/objx v0.2.0 // indirect
 	github.com/swaggo/gin-swagger v1.2.0
 	github.com/swaggo/swag v1.6.5
 	github.com/tebeka/strftime v0.1.3 // indirect
