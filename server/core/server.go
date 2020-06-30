9d9d03dd86e48722008438f165112be0a260f52a
lvan100 <lvan100@yeah.net>
2020-06-30 23:18:52 +0800

    上一版集成的结果不对，很多中间件没有集成进去，换一种方法

--- server/core/server.go
+++ server/core/server.go
@@ -1,25 +1,14 @@
 package core
 
 import (
-	"errors"
-	"net/http"
-
 	"gin-vue-admin/global"
 	"gin-vue-admin/initialize"
 
 	"github.com/go-spring/go-spring-web/spring-gin"
 	"github.com/go-spring/go-spring-web/spring-web"
 	"github.com/go-spring/go-spring/spring-boot"
-	"github.com/go-spring/go-spring/starter-web"
+	_ "github.com/go-spring/go-spring/starter-gin"
 )
-
-func init() {
-	SpringBoot.RegisterBeanFn(func(config WebStarter.WebServerConfig) SpringWeb.WebContainer {
-		c := SpringGin.NewContainer(SpringWeb.ContainerConfig{Port: config.Port})
-		c.SetEnableSwagger(false)
-		return c
-	})
-}
 
 func RunWindowsServer() {
 	if global.GVA_CONFIG.System.UseMultipoint {
@@ -29,23 +18,6 @@ func RunWindowsServer() {
 
 	Router := initialize.Routers()
 	Router.Static("/form-generator", "./resource/page")
-
-	for _, info := range Router.Routes() {
-		switch info.Method {
-		case http.MethodGet:
-			SpringBoot.GET(info.Path, SpringGin.Gin(info.HandlerFunc))
-		case http.MethodPost:
-			SpringBoot.POST(info.Path, SpringGin.Gin(info.HandlerFunc))
-		case http.MethodDelete:
-			SpringBoot.DELETE(info.Path, SpringGin.Gin(info.HandlerFunc))
-		case http.MethodPut:
-			SpringBoot.PUT(info.Path, SpringGin.Gin(info.HandlerFunc))
-		case http.MethodHead:
-			SpringBoot.HEAD(info.Path, SpringGin.Gin(info.HandlerFunc))
-		default:
-			panic(errors.New("unsupported http method " + info.Method))
-		}
-	}
 
 	// 插件安装 暂时只是后台功能 添加model 添加路由 添加对数据库的操作  详细插件测试模板可看https://github.com/piexlmax/gvaplug  此处不建议投入生产
 	//err := initialize.InstallPlug(global.GVA_DB, Router, gvaplug.GvaPlug{})
@@ -73,5 +45,12 @@ func RunWindowsServer() {
 	//默认前端文件运行地址:http://127.0.0.1:8080`, s.Addr)
 
 	// global.GVA_LOG.Error(s.ListenAndServe())
+
+	SpringBoot.Config(func(c SpringWeb.WebContainer) {
+		gc := c.(*SpringGin.Container)
+		gc.SetEnableSwagger(false)
+		gc.SetGinEngine(Router)
+	})
+
 	SpringBoot.RunApplication("./")
 }
--- server/initialize/router.go
+++ server/initialize/router.go
@@ -5,22 +5,25 @@ import (
 	"gin-vue-admin/global"
 	"gin-vue-admin/middleware"
 	"gin-vue-admin/router"
+
 	"github.com/gin-gonic/gin"
 	"github.com/swaggo/gin-swagger"
 	"github.com/swaggo/gin-swagger/swaggerFiles"
 )
 
 // 初始化总路由
-
 func Routers() *gin.Engine {
 	var Router = gin.Default()
 	// Router.Use(middleware.LoadTls())  // 打开就能玩https了
 	global.GVA_LOG.Debug("use middleware logger")
+
 	// 跨域
 	Router.Use(middleware.Cors())
 	global.GVA_LOG.Debug("use middleware cors")
+
 	Router.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	global.GVA_LOG.Debug("register swagger handler")
+
 	// 方便统一添加路由组前缀 多服务器上线使用
 	ApiGroup := Router.Group("")
 	router.InitUserRouter(ApiGroup)                  // 注册用户路由
